8ca63ee565e2f85c09b044feab24b1be7d16a79e
Nima Bargestan <nimam700@gmail.com>
2023-06-02 23:53:03 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <main className="flex justify-center">
+        <h1 className="text-3xl font-bold">Init</h1>
+      </main>
+    </>
+  );
+}
+
+export default App;
